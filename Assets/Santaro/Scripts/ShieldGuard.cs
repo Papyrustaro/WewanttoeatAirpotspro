3efe2674c7e79aa9c6a796ce9cddc6a08aeb5c81
Papyrustaro <undertale1104@gmail.com>
2020-09-12 22:13:43 +0900

    create-guard

---
+++ Assets/Santaro/Scripts/ShieldGuard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// アタッチすると、Sでガードできる。
+/// </summary>
+public class ShieldGuard : MonoBehaviour
+{
+    [SerializeField] private GameObject shieldObject;
+    [SerializeField] private Vector3 shieldRightLocalPositionInGurad;
+    [SerializeField] private BoxCollider2D shieldCollider;
+    [SerializeField] private Movement movement;
+    private Vector3 defaultShieldLocalPosition;
+
+    private void Awake()
+    {
+        this.defaultShieldLocalPosition = this.shieldObject.transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.movement.canMove = false;
+            this.shieldCollider.enabled = true;
+            if(movement.currentBodyDirection == Movement.BodyDirection.Right)
+            {
+                this.shieldObject.transform.localPosition = this.shieldRightLocalPositionInGurad;
+            }
+            else
+            {
+                this.shieldObject.transform.localPosition = new Vector3(-this.shieldRightLocalPositionInGurad.x, this.shieldRightLocalPositionInGurad.y, 0f);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            this.shieldObject.transform.localPosition = this.defaultShieldLocalPosition;
+            this.movement.canMove = true;
+            this.shieldCollider.enabled = false;
+        }
+    }
+}
--- Assets/Santaro/Scripts/SwordAttack.cs
+++ Assets/Santaro/Scripts/SwordAttack.cs
@@ -13,7 +13,6 @@ public class SwordAttack : MonoBehaviour, IAttack
     [SerializeField] private GameObject swordObject;
     [SerializeField] private GameObject playerObject;
     [SerializeField] private float oneSlashTime = 0.5f;
-    [SerializeField] private bool isRight = true; //とりあえず
     private bool canSlash = true;
 
     public void Attack(Movement movement, Action onFinish)
@@ -22,7 +21,7 @@ public class SwordAttack : MonoBehaviour, IAttack
         this.canSlash = false;
         Vector3 defaultLocalSwordPosition = this.swordObject.transform.localPosition;
         Quaternion defaultLocalSwordRotation = this.swordObject.transform.localRotation;
-        if (this.isRight)
+        if (movement.currentBodyDirection == Movement.BodyDirection.Right)
         {
             this.swordObject.transform.localPosition = this.initSwordLocalPositionInRight;
             StartCoroutine(SantaroTransformManager.RotateInCertainTimeByAxisFromAway(this.swordObject.transform, this.playerObject.transform, -120f, this.oneSlashTime, () => {
@@ -44,6 +43,5 @@ public class SwordAttack : MonoBehaviour, IAttack
         }
         
     }
-
 
 }
