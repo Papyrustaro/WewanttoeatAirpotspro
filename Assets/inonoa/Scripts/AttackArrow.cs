a782cdcb0c18df61d33edbc4b7c030b2add027dc
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-09-12 23:59:01 +0900

    矢

---
+++ Assets/inonoa/Scripts/AttackArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DG.Tweening;
+
+public class AttackArrow : MonoBehaviour, IAttack
+{
+    [SerializeField] Arrow arrowPrefab;
+
+    public void Attack(Movement movement, Action onFinished)
+    {
+        Arrow arrow = Instantiate(arrowPrefab, transform);
+        arrow.OnThrown(movement.currentBodyDirection == Movement.BodyDirection.Right);
+
+        DOVirtual.DelayedCall(0.2f, () => 
+        {
+            arrow.GetComponent<Collider2D>().enabled = true;
+            onFinished.Invoke();
+        });
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
