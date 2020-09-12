91f7f581f9be5b0457d89eba15f16d4b4a25ace3
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-09-12 22:23:00 +0900

    Movementの向きに応じて投げる方向を変える

--- Assets/inonoa/Scripts/AttackBoomerang.cs
+++ Assets/inonoa/Scripts/AttackBoomerang.cs
@@ -16,11 +16,14 @@ public class AttackBoomerang : MonoBehaviour, IAttack
     public void Attack(Movement movement, Action onFinished)
     {
         Boomerang bmrg = Instantiate(boomerang, this.transform);
-        bmrg.OnThrown(false, () =>
-        {
-            Destroy(bmrg.gameObject);
-            onFinished.Invoke();
-        });
+        bmrg.OnThrown(
+            movement.currentBodyDirection == Movement.BodyDirection.Right, 
+            () =>
+            {
+                Destroy(bmrg.gameObject);
+                onFinished.Invoke();
+            }
+        );
         
         //出現した瞬間自身と衝突して消滅するのを回避、かなりその場しのぎ
         DOVirtual.DelayedCall(0.2f, () => bmrg.GetComponent<Collider2D>().enabled = true);
