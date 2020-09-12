0ad7df7797c569b7d201a928e542129a08212e48
Papyrustaro <undertale1104@gmail.com>
2020-09-13 08:00:58 +0900

    create PlayerCharacterBase

---
+++ Assets/Santaro/Scripts/LassoAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 投げ縄生成と、初期速度代入。
+/// </summary>
+public class LassoAttack : MonoBehaviour, IAttack
+{
+    [SerializeField] private GameObject lassoObject;
+    [SerializeField] private Transform rightLassoInstantiatePosition;
+    [SerializeField] private Transform leftLassoInstantiatePosition;
+
+    public void Attack(Movement movement, Action onFinish)
+    {
+        if(movement.currentBodyDirection == Movement.BodyDirection.Left)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Santaro/Scripts/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public static PlayerCharacter Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            throw new System.Exception();
+        }
+    }
+}
