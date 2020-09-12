e5f91793c3e2188d9bdf179c7be9c720d7e232d9
tmizuho <takato@cyber.t.u-tokyo.ac.jp>
2020-09-12 18:46:21 +0900

    movement interface and player movement

---
+++ Assets/Mizuho/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    public float moveSpeed = 0.1f;
+    public float jumpForce = 300f;
+    public bool isJumping = false;
+
+
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    virtual protected void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    virtual protected void Update()
+    {
+        
+    }
+
+    public void LeftMove()
+    {
+        transform.localPosition += Vector3.left * moveSpeed;
+    }
+
+    public void RightMove()
+    {
+        transform.localPosition += Vector3.right * moveSpeed;
+    }
+
+    public void Jump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            rb2D.AddForce(transform.up * jumpForce);
+        }
+
+    }
+
+    public void Guard()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.name=="Stage" && isJumping)
+        {
+            isJumping = false;
+        }
+    }
+}
---
+++ Assets/Mizuho/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Movement
+{
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    override protected void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            LeftMove();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Guard();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            RightMove();
+        }
+    }
+
+}
