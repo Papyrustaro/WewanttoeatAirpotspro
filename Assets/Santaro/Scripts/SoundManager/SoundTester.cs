b98d4999b16e6b94f33b710c3359e5ee589e76aa
Papyrustaro <undertale1104@gmail.com>
2020-09-13 13:53:20 +0900

    create audioManager

---
+++ Assets/Santaro/Scripts/SoundManager/SoundTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボタンの入力でサウンドを鳴らすサンプル
+/// </summary>
+public class SoundTester : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SEManager.PlaySE(SEManager.E_SE.Slash);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SEManager.PlaySE(SEManager.E_SE.Jump);
+        }
+    }
+}
