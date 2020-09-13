9aac88c6fb822403631b3a2a2ad31995aef62700
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-09-13 15:40:35 +0900

    1

---
+++ Assets/inonoa/Scripts/FlipEnemySprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipEnemySprite : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rigidbody.velocity.x == 0) return;
+        renderer.flipX = rigidbody.velocity.x > 0;
+    }
+}
